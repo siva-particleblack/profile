f61515d2a1e528e783a526203f33070de8c9380c
siva-PB <sivaramakrishnan.rengaraj@particleblack.com>
2024-03-19 11:54:08 +0530

    10
    10

--- lib/pics-profile/profile/profile.component.d.ts
+++ lib/pics-profile/profile/profile.component.d.ts
@@ -45,8 +45,6 @@ export declare class ProfileComponent implements OnInit {
     orgId: any;
     errors: string[];
     httpService: any;
-    buildVersion: any;
-    buildTimestamp: any;
     constructor(injector: Injector, authService: AuthService, formBuilder: FormBuilder, profileService: ProfileService, attachmentService: AttachmentsService, _storeservice: DataStoreService, confirmationService: ConfirmationService, profileUpdateService: ProfileUpdateService);
     ngOnInit(): void;
     initializeResetPasswordForm(): void;
