b49f7792883e409c35138792c3133dacb10a0ba3
siva-PB <sivaramakrishnan.rengaraj@particleblack.com>
2024-03-21 13:17:45 +0530

    24

--- lib/pics-profile/@core/service/navigation-alert.service.d.ts
+++ lib/pics-profile/@core/service/navigation-alert.service.d.ts
@@ -3,10 +3,10 @@ import { Subject } from 'rxjs';
 import * as i0 from "@angular/core";
 export declare class NavigationAlertService {
     private router;
-    private showAlertSubject;
+    private navigationSubject;
     constructor(router: Router);
     private init;
-    getShowAlertSubject(): Subject<boolean>;
+    getNavigationSubject(): Subject<boolean>;
     static ɵfac: i0.ɵɵFactoryDeclaration<NavigationAlertService, never>;
     static ɵprov: i0.ɵɵInjectableDeclaration<NavigationAlertService>;
 }
